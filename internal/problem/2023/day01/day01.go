0fff5d3c373068796c359ddffdb0f70445688421
Jeff Weber <jweber@cofront.net>
2023-12-01 00:33:16 -0500

    2023 day 1

--- internal/problem/2023/day01/day01.go
+++ internal/problem/2023/day01/day01.go
@@ -1,6 +1,11 @@
 package day01
 
 import (
+	"fmt"
+	"strconv"
+	"strings"
+	"unicode"
+
 	"github.com/jbweber/advent-of-code-go/internal"
 )
 
@@ -19,9 +24,63 @@ func Execute(input string) (string, string, error) {
 }
 
 func part1(input string) (string, error) {
-	return "", nil
+	total := 0
+	for _, line := range strings.Split(input, "\n") {
+		f := 'f'
+		l := 'l'
+		for i := 0; i < len(line); i++ {
+			if unicode.IsDigit(rune(line[i])) == true {
+				if f == 'f' {
+					f = rune(line[i])
+				}
+				l = rune(line[i])
+
+			}
+		}
+
+		d, _ := strconv.Atoi(string(f) + string(l))
+		total += d
+	}
+	return fmt.Sprint(total), nil
+}
+
+var nums = map[string]rune{
+	"one":   '1',
+	"two":   '2',
+	"three": '3',
+	"four":  '4',
+	"five":  '5',
+	"six":   '6',
+	"seven": '7',
+	"eight": '8',
+	"nine":  '9',
 }
 
 func part2(input string) (string, error) {
-	return "", nil
+	total := 0
+	for _, line := range strings.Split(input, "\n") {
+		f := 'f'
+		l := 'l'
+		for i := 0; i < len(line); i++ {
+			for k, v := range nums {
+				if strings.HasPrefix(line[i:], k) {
+					if f == 'f' {
+						f = v
+					}
+					l = v
+					continue
+				}
+			}
+			if unicode.IsDigit(rune(line[i])) == true {
+				if f == 'f' {
+					f = rune(line[i])
+				}
+				l = rune(line[i])
+			}
+		}
+		d, _ := strconv.Atoi(string(f) + string(l))
+		total += d
+	}
+
+	return fmt.Sprint(total), nil
 }
