be6aa104ebf52f2f53733130a3cf4400ae375a59
Jeff Weber <jweber@cofront.net>
2023-12-03 00:41:21 -0500

    2023 day 3 part 1

--- internal/problem/2023/day03/day03.go
+++ internal/problem/2023/day03/day03.go
@@ -1,6 +1,11 @@
 package day03
 
 import (
+	"fmt"
+	"strconv"
+	"strings"
+	"unicode"
+
 	"github.com/jbweber/advent-of-code-go/internal"
 )
 
@@ -19,9 +24,121 @@ func Execute(input string) (string, string, error) {
 }
 
 func part1(input string) (string, error) {
-	return "", nil
+	data := strings.Split(input, "\n")
+	total := 0
+	for i := 0; i < len(data); i++ {
+		adjacent := false
+		inDigit := false
+		num := ""
+		for j := 0; j < len(data[i]); j++ {
+			if unicode.IsDigit(rune(data[i][j])) {
+				inDigit = true
+				num += string(data[i][j])
+
+				// check adjacent
+				if !adjacent {
+
+					adjacent = IsAdjacent(data, j, i)
+				}
+				if j+1 != len(data[i]) {
+					continue
+				}
+			}
+
+			if inDigit == true {
+				fmt.Println(num, adjacent)
+
+				if adjacent {
+					v, _ := strconv.Atoi(num)
+					total += v
+				}
+
+				adjacent = false
+				inDigit = false
+				num = ""
+			}
+		}
+	}
+
+	return fmt.Sprint(total), nil
 }
 
 func part2(input string) (string, error) {
 	return "", nil
+}
+
+func IsAdjacent(data []string, x, y int) bool {
+	// x,y+1
+	if y < len(data)-1 {
+		val := data[y+1][x]
+		if !unicode.IsDigit(rune(val)) && val != '.' {
+			fmt.Println(string(val))
+			return true
+		}
+	}
+
+	// x-1,y
+	if x > 0 {
+		val := data[y][x-1]
+		if !unicode.IsDigit(rune(val)) && val != '.' {
+			fmt.Println(string(val))
+			return true
+		}
+	}
+
+	// x+1,y
+	if x < len(data[y])-1 {
+		val := data[y][x+1]
+		if !unicode.IsDigit(rune(val)) && val != '.' {
+			fmt.Println(string(val))
+			return true
+		}
+	}
+
+	// x,y-1
+	if y > 0 {
+		val := data[y-1][x]
+		if !unicode.IsDigit(rune(val)) && val != '.' {
+			fmt.Println(string(val))
+			return true
+		}
+	}
+
+	// x+1,y+1
+	if y < len(data)-1 && x < len(data[y])-1 {
+		val := data[y+1][x+1]
+		if !unicode.IsDigit(rune(val)) && val != '.' {
+			fmt.Println(string(val))
+			return true
+		}
+	}
+
+	// x-1,y+1
+	if y < len(data)-1 && x > 0 {
+		val := data[y+1][x-1]
+		if !unicode.IsDigit(rune(val)) && val != '.' {
+			fmt.Println(string(val))
+			return true
+		}
+	}
+
+	// x+1,y-1
+	if y > 0 && x < len(data[y])-1 {
+		val := data[y-1][x+1]
+		if !unicode.IsDigit(rune(val)) && val != '.' {
+			fmt.Println(string(val))
+			return true
+		}
+	}
+
+	// x-1,y-1
+	if y > 0 && x > 0 {
+		val := data[y-1][x-1]
+		if !unicode.IsDigit(rune(val)) && val != '.' {
+			fmt.Println(string(val))
+			return true
+		}
+	}
+
+	return false
 }
---
+++ internal/problem/2023/day03/day03_test.go
@@ -0,0 +1,93 @@
+package day03
+
+import "testing"
+
+func TestIsAdjacent(t *testing.T) {
+	testCases := []struct {
+		name  string
+		input []string
+		x     int
+		y     int
+		is    bool
+	}{
+		{
+			name:  "x",
+			input: []string{"5.", ".*"},
+			x:     0,
+			y:     0,
+			is:    true,
+		},
+		{
+			name:  "no",
+			input: []string{"...", ".5.", "..."},
+			x:     1,
+			y:     1,
+			is:    false,
+		},
+		{
+			name:  "x,y+1",
+			input: []string{"...", ".5.", ".*."},
+			x:     1,
+			y:     1,
+			is:    true,
+		},
+		{
+			name:  "x-1,y",
+			input: []string{"...", "*5.", "..."},
+			x:     1,
+			y:     1,
+			is:    true,
+		},
+		{
+			name:  "x+1,y",
+			input: []string{"...", ".5*", "..."},
+			x:     1,
+			y:     1,
+			is:    true,
+		},
+		{
+			name:  "x,y-1",
+			input: []string{".*.", ".5.", "..."},
+			x:     1,
+			y:     1,
+			is:    true,
+		},
+		{
+			name:  "x+1,y+1",
+			input: []string{"...", ".5.", "..*"},
+			x:     1,
+			y:     1,
+			is:    true,
+		},
+		{
+			name:  "x-1,y+1",
+			input: []string{"...", ".5.", "*.."},
+			x:     1,
+			y:     1,
+			is:    true,
+		},
+		{
+			name:  "x+1,y-1",
+			input: []string{"..*", ".5.", "..."},
+			x:     1,
+			y:     1,
+			is:    true,
+		},
+		{
+			name:  "x-1,y-1",
+			input: []string{"*..", ".5.", "..."},
+			x:     1,
+			y:     1,
+			is:    true,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			r := IsAdjacent(tc.input, tc.x, tc.y)
+			if r != tc.is {
+				t.Fail()
+			}
+		})
+	}
+}
