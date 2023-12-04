ca9656a3ddb78ec6501d206ad4a6dafbeb664a59
Jeff Weber <jweber@cofront.net>
2023-12-04 00:14:19 -0500

    2023 day 4 part 1

--- internal/problem/2023/day04/day04.go
+++ internal/problem/2023/day04/day04.go
@@ -1,6 +1,9 @@
 package day04
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/jbweber/advent-of-code-go/internal"
 )
 
@@ -19,9 +22,50 @@ func Execute(input string) (string, string, error) {
 }
 
 func part1(input string) (string, error) {
-	return "", nil
+	lines := strings.Split(input, "\n")
+	total := 0
+	for _, line := range lines {
+		total += parseLine1(line)
+	}
+	return fmt.Sprint(total), nil
 }
 
 func part2(input string) (string, error) {
 	return "", nil
+}
+
+func parseLine1(line string) int {
+	parts1 := strings.Split(line, " | ")
+	parts2 := strings.Split(parts1[0], ":")
+	//parts3 := strings.Fields(parts2[0])
+	//id := parts3[1]
+	winners := strings.Fields(parts2[1])
+	numbers := strings.Fields(parts1[1])
+
+	unions := map[string]int{}
+
+	for _, v := range winners {
+		unions[v] = 0
+	}
+
+	count := 0
+	for _, v := range numbers {
+		_, ok := unions[v]
+		if ok {
+			count += 1
+		}
+	}
+
+	total := 0
+	for i := 0; i < count; i++ {
+		if total == 0 {
+			total = 1
+			continue
+		}
+		total = total * 2
+	}
+	return total
+}
+
+type card struct {
 }
