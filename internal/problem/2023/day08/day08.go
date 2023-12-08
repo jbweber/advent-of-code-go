4552f06d6698e143adcdc0be10099eb19d9c6d6b
Jeff Weber <jweber@cofront.net>
2023-12-08 00:43:41 -0500

    2023 day 8 part 2
    lucky guess that lcm worked. inspecting the data after trying
    shows that the paths are all cycles which means it is an okay
    approach. previous aoc where I used an lcm made me guess it
    here. i'd like to explore a more generic solution though as
    time permits.

--- internal/problem/2023/day08/day08.go
+++ internal/problem/2023/day08/day08.go
@@ -51,7 +51,55 @@ func part1(input string) (string, error) {
 }
 
 func part2(input string) (string, error) {
-	return "", nil
+	d, p := parse1(input)
+
+	// find starts
+	var nexts []string
+	for k, _ := range p {
+		if strings.HasSuffix(k, "A") {
+			nexts = append(nexts, k)
+		}
+	}
+
+	var turns []int
+	var starts []string
+
+	for k, _ := range p {
+		if strings.HasSuffix(k, "A") {
+			starts = append(starts, k)
+		}
+	}
+
+	for _, start := range starts {
+
+		count := 0
+		next := start
+		for i := 0; i < len(d); i++ {
+			count += 1
+
+			opts := p[next]
+
+			if d[i] == "L" {
+				next = opts.Left
+			} else {
+				next = opts.Right
+			}
+
+			if strings.HasSuffix(next, "Z") {
+				break
+			}
+
+			if i+1 == len(d) {
+				i = -1
+			}
+		}
+
+		turns = append(turns, count)
+	}
+
+	r := getLCM(turns)
+
+	return fmt.Sprint(r), nil
 }
 
 func parse1(input string) ([]string, map[string]Instruction) {
@@ -76,4 +124,35 @@ func parse1(input string) ([]string, map[string]Instruction) {
 type Instruction struct {
 	Left  string
 	Right string
+}
+
+// https://en.wikipedia.org/wiki/Greatest_common_divisor
+// Euclidean algorithm
+func gcd(a, b int) int {
+	for b != 0 {
+		a, b = b, a%b
+	}
+	return a
+}
+
+// https://en.wikipedia.org/wiki/Least_common_multiple
+func lcm(a, b int) int {
+	if a == 0 && b == 0 {
+		return 0
+	}
+
+	return a * b / gcd(a, b)
+}
+
+func getLCM(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+
+	result := nums[0]
+	for i := 1; i < len(nums); i++ {
+		result = lcm(result, nums[i])
+	}
+
+	return result
 }
