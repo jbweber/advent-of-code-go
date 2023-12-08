273daffea50ff55310d91487dd8f936b5b737d38
Jeff Weber <jweber@cofront.net>
2023-12-08 00:12:38 -0500

    2023 day 8 part 1

--- internal/problem/2023/day08/day08.go
+++ internal/problem/2023/day08/day08.go
@@ -1,6 +1,9 @@
 package day08
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/jbweber/advent-of-code-go/internal"
 )
 
@@ -19,9 +22,58 @@ func Execute(input string) (string, string, error) {
 }
 
 func part1(input string) (string, error) {
-	return "", nil
+
+	d, p := parse1(input)
+
+	turns := 0
+	next := "AAA"
+	for i := 0; i < len(d); i++ {
+		turns += 1
+
+		opts := p[next]
+
+		if d[i] == "L" {
+			next = opts.Left
+		} else {
+			next = opts.Right
+		}
+
+		if next == "ZZZ" {
+			break
+		}
+
+		if i+1 == len(d) {
+			i = -1
+		}
+	}
+
+	return fmt.Sprint(turns), nil
 }
 
 func part2(input string) (string, error) {
 	return "", nil
+}
+
+func parse1(input string) ([]string, map[string]Instruction) {
+	lines := strings.Split(input, "\n")
+
+	directions := strings.Split(lines[0], "")
+
+	paths := map[string]Instruction{}
+	for _, line := range lines[2:] {
+		parts1 := strings.Split(line, " = ")
+		start := parts1[0]
+		other := strings.TrimLeft(parts1[1], "(")
+		other = strings.TrimRight(other, ")")
+		parts2 := strings.Split(other, ", ")
+
+		paths[start] = Instruction{Left: parts2[0], Right: parts2[1]}
+	}
+
+	return directions, paths
+}
+
+type Instruction struct {
+	Left  string
+	Right string
 }
